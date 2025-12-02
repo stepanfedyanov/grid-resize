a465074affe56155de424a7c917f512e92d081b1
Stepan Fedyanov <65213304+stepanfedyanov@users.noreply.github.com>
2025-12-02 19:54:40 +0000

    Add Github Pages deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,9 @@ export default defineNuxtConfig({
   css: [
     '@/assets/css/main.scss',
   ],
+  app: {
+    baseURL: process.env.NODE_ENV === 'production' ? '/grid-resize/' : '/',
+    buildAssetsDir: 'assets',
+  },
+  ssr: false,
 })
